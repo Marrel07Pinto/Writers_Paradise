4d639209cca0cfc022b442ac24ea6f25f57636d2
marrel <marrel7pinto@gmail.com>
2023-11-02 08:39:06 +0000

    Added attributes to books table

---
+++ database/migrations/2023_11_02_083352_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('b_name');
+            $table->string('b_description');
+            $table->string('b_genre');
+            $table->Integer('b_price');
+            $table->Integer('b_no_of_copies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
