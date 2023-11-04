968f038232978353c66d4ff86ef65815c92e2d35
marrel <marrel7pinto@gmail.com>
2023-11-04 22:53:38 +0000

    modified to enter data into database

---
+++ database/seeders/OrderTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+Use App\Models\Order;
+
+class OrderTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $o = new Order;
+        $o -> writer_id ='1';
+        $o->save();
+        Order::factory()->count(10)->create();
+    }
+}
