f55de1d1c72b10cfd2dc24e238eb5bae01c16ce5
marrel <marrel7pinto@gmail.com>
2023-11-26 23:21:41 +0000

    added a namespace Writer

--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 Use App\Models\Book;
+Use App\Models\Writer;
 
 class BookTableSeeder extends Seeder
 {
