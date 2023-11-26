da7e59d1aa555528f2f743bebc9f3731618c41b6
marrel <marrel7pinto@gmail.com>
2023-11-26 22:54:13 +0000

    created a pivot table

---
+++ database/migrations/2023_11_26_214017_create_writers_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('writers_books',function(Blueprint $table){
+            $table->primary(['writer_id','book_id']);
+            $table->unsignedBigInteger('writer_id');
+            $table->unsignedBigInteger('book_id');
+            $table->timestamps();
+
+            $table->foreign('writer_id')->references('writer_id')->on('writers')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('book_id')->references('book_id')->on('books')
+            ->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
