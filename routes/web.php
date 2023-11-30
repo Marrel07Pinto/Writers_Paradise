b30846de389d6af2c9d0958496ddfc1a0ba64a54
marrel <marrel7pinto@gmail.com>
2023-11-30 11:03:23 +0000

    created a link to homepage

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/Home', function () {
+    return view('Home');
 });
 Route::get('/Ebooks', function () {
     $books = DB::table('books')->get();
