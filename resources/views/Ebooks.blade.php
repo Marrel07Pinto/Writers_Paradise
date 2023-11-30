49a42a2466b428d95a05c178bea24d2443508f98
marrel <marrel7pinto@gmail.com>
2023-11-30 10:25:00 +0000

    changed the dash board

--- resources/views/Ebooks.blade.php
+++ resources/views/Ebooks.blade.php
@@ -57,10 +57,10 @@
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#menu">E-Books</a></li>
-          <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#chefs">E-Books</a></li>
+          <li><a class="nav-link scrollto" href="#gallery">Chat</a></li>
+          <li><a class="nav-link scrollto" href="#gallery">Profile</a></li>
+          <!-- <li><a class="nav-link scrollto" href="#contact">Contact</a></li> -->
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
