3522a9ee35cf2c5488ca8cedcf944eb5aebeb84c
marrel <marrel7pinto@gmail.com>
2023-12-13 21:58:53 +0000

    removed space

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,8 +49,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+
         // Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect(RouteServiceProvider::LOGIN);
     }
 }
