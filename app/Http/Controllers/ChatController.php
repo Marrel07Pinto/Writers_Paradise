651b7d66a0cb970784f6aaf9d0639d1ccb1a8f7a
marrel <marrel7pinto@gmail.com>
2023-12-13 21:58:18 +0000

    corrected validation

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -51,7 +51,7 @@ class ChatController extends Controller
 
         $request->validate([
             'p_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'Post Caption'=> 'required|String',
+            'p_Caption'=> 'required|String',
             
         ]);
     
